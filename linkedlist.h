c050b2bf87946f95cd1f4b42f67f3819f01ee0dc
Mateo Ismael Bernales Coloma <choladero@gmail.com>
2021-04-14 08:59:50 -0500

    Update linkedlist.h

--- linkedlist.h
+++ linkedlist.h
@@ -30,13 +30,153 @@ template <typename T>
 void LinkedList<T>::front(T &elem)
 {
 
+// Comments #3
+{
+template <class T>
+class LinkedList {
+	public:
+		LinkedList() : size_(0), begin_(NULL), end_(NULL) {} 
+		LinkedList(const LinkedList& other);
+	
+		~LinkedList();
+
+		LinkedList<T> operator=(const LinkedList& other);
+
+		T front() { return *begin_; }
+		T front() const { return *begin_; }
+	
+		T back() { return *end_; }
+		T back() const { return *end_; }
+	
+		ListIterator<T> begin();
+		ListIterator<T> end();
+
+		void push_back(const T&);
+		void pop_back();
+		
+		void push_front(const T&);
+		void pop_front();
+		
+		void insert(ListIterator<T>, const T&);
+
+	private:
+		ListNode<T>* begin_;
+		ListNode<T>* end_;
+		int size_;
+
+};
+
+template <class T>
+ListIterator<T> LinkedList<T>::end() {
+	ListIterator<T> e(end_);
+	return e;
 }
 
-// Comments #3
-template <typename T>
-T LinkedList<T>::back()
-{
+template <class T>
+ListIterator<T> LinkedList<T>::begin() {
+	ListIterator<T> t(begin_);
+	return t;
+}
 
+template <class T>
+LinkedList<T>::~LinkedList() {
+	ListNode<T>* ptr = begin_;
+	while ( ptr->next != NULL ) {
+		ptr = ptr->next;
+		delete ptr->prev;
+	}
+	delete ptr;
+}
+
+template <class T>
+LinkedList<T> LinkedList<T>::operator=(const LinkedList& other) {
+	ListNode<T>* ptr;
+	LinkedList<T> new_list;
+
+	for ( ptr = other.begin_; ptr != NULL; ptr = ptr->next ) {
+        new_list.push_back(*ptr);
+    }
+	return new_list;
+}
+
+template <class T>
+LinkedList<T>::LinkedList(const LinkedList& other) {
+	ListNode<T>* ptr;
+	for ( ptr = other.begin_ ; ptr != NULL; ptr = ptr->next ) {
+		push_back(*ptr);
+	}
+}
+
+template <class T>
+void LinkedList<T>::insert(ListIterator<T> ptr, const T& element) {
+	ListNode<T>* new_node = new ListNode<T>;
+
+	new_node->element = element;
+	
+	new_node->prev = ptr->prev;
+	ptr->prev->next = new_node;
+	new_node->next = ptr;
+	ptr->prev = new_node;
+	
+	size_++;
+}	
+
+template <class T>
+void LinkedList<T>::push_front(const T& element) {
+	ListNode<T>* new_node = new ListNode<T>;
+	new_node->element = element;
+	new_node->next = begin_;
+	new_node->prev = NULL;
+
+	if (begin_ == NULL) {
+		begin_ = new_node;
+	} else {
+		begin_->prev = new_node;
+		begin_ = new_node;
+	}
+	
+	if ( end_ == NULL ) {
+		end_ = new_node;
+	}
+	
+	size_++;
+}
+
+template <class T>
+void LinkedList<T>::pop_front() {
+	size_--;
+	begin_ = begin_->next;
+	delete begin_->prev;
+	begin_->prev = NULL;
+}
+
+template <class T>
+void LinkedList<T>::push_back(const T& element) {
+	ListNode<T>* new_node = new ListNode<T>;
+	new_node->element = element;		
+	new_node->prev = end_;
+	new_node->next = NULL;
+
+	if ( end_ == NULL ) {
+		end_ = new_node;
+	} else {
+		end_->next = new_node;
+		end_ = new_node;
+	}
+
+	if ( begin_ == NULL ) {
+		begin_ = new_node;
+	}
+
+	size_++;
+}
+
+template <class T>
+void LinkedList<T>::pop_back() {
+	size_--;
+	end_ = end_->prev;
+	delete end_->next;
+	end_->next = NULL;
 }
 
 #endif
